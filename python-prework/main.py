2090d8619e01ab46fc3feec8043779d52ad5eeb2
Nico Pinguel <nico.mp215@gmail.com>
2021-10-11 14:11:42 -0500

    more changes

--- python-prework/main.py
+++ python-prework/main.py
@@ -59,6 +59,7 @@
 # Write a function to check to see if all numbers in list are consecutive numbers. 
 # For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. 
 # The return should be boolean Type.
+#testing 
 
 def is_consecutive(a_list):
     return sorted(a_list) == list(range(min(a_list),max(a_list)+1))
